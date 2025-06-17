f528ef297b4d721ae8d0abec6ee394f6b0c778e8
Melyn Were <WereMA@cardiff.ac.uk>
2025-06-17 17:17:12 +0100

    import fastapi and create instance

---
+++ book_api.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# book_catalog(id, title: str, author: str){
+#         [
+#             1, "Rules of Life", "John Doe",
+#             1, "Rules of Life", "John Doe",
+#             1, "Rules of Life", "John Doe",
+#             1, "Rules of Life", "John Doe",
+#             1, "Rules of Life", "John Doe"
+
+#         ]
+# }
+
+
+
