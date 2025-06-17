41ee3e44b8d6b92066994df975d670bb44ad17b2
Melyn Were <WereMA@cardiff.ac.uk>
2025-06-17 15:42:23 +0100

    add get and post methods

--- api.py
+++ api.py
@@ -3,5 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/get-message")
-async def read_msg():
+async def read_root():
     return {"Message":"Congrats! This is your first API"}
+
+@app.get("/pass-name")
+def hello(name: str):
+    return {"Message": "My name is " + name}
+
+initial_string = "Never Give Up"
+
+@app.post("/add")
+async def add_text(text: str):
+    global initial_string
+    initial_string += text
+    return{"Message":"Text added","current_string": text}
