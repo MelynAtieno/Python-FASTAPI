1dec13947c7d3be8ab00d45a808fe51a34c6e84d
Melyn Were <WereMA@cardiff.ac.uk>
2025-06-17 15:57:38 +0100

    add put request

--- api.py
+++ api.py
@@ -16,4 +16,11 @@ initial_string = "Never Give Up"
 async def add_text(text: str):
     global initial_string
     initial_string += text
-    return{"Message":"Text added","current_string": text}
+    return {"Message":"Text added","current_string": initial_string}
+
+@app.put("/change")
+async def change_text(new_text: str):
+    global initial_string
+    initial_string = new_text
+    return {"Message": "Text changed","current_string": initial_string}
+
