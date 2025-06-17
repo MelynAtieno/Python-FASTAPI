6be73cf8c235697f4ed9ffe67dfdb0f943e11a6f
Melyn Were <WereMA@cardiff.ac.uk>
2025-06-17 17:28:22 +0100

    add put, get and post methods

--- book_api.py
+++ book_api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# book_catalog(id, title: str, author: str){
+# books(id, title: str, author: str){
 #         [
 #             1, "Rules of Life", "John Doe",
 #             1, "Rules of Life", "John Doe",
@@ -12,6 +12,26 @@ app = FastAPI()
 
 #         ]
 # }
+
+@app.get("/books")
+async def get_books():
+    return {"Message":"Success!", "Books": books}
+
+@app.post("/books")
+async def post_book(id: int, name: str, author: str):
+    global books
+    books += id,name,author
+    return {"Message": "Book added successfully!", "Books": books}
+
+@app.get("/books/{id}")
+async def get_book_id():
+    return {"Message":"Success", "Book": {id}}
+
+@app.put(/"books/{id}")
+async def edit_book():
+    global books
+    books[id] = 
+    return {"Message": "Book edited successfully!", "Books": books}
 
 
 
