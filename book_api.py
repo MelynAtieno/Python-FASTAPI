b9ba6b85f564c0a7b8f8fc4ebeefcafba5f7d9d4
Melyn Were <WereMA@cardiff.ac.uk>
2025-06-18 15:30:31 +0100

    add book examples to generated json schema

--- book_api.py
+++ book_api.py
@@ -1,17 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# books(id, title: str, author: str){
-#         [
-#             1, "Rules of Life", "John Doe",
-#             1, "Rules of Life", "John Doe",
-#             1, "Rules of Life", "John Doe",
-#             1, "Rules of Life", "John Doe",
-#             1, "Rules of Life", "John Doe"
+class Books(BaseModel):
+    name: str
+    author: str
 
-#         ]
-# }
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name" : "Rules of Life",
+                    "author" : "John Doe"
+                },
+                {
+                    "name" : "Happy life",
+                    "author" : "James Bennett"
+                }
+            ]
+        }
+    }
+        
+           
+    
 
 @app.get("/books")
 async def get_books():
@@ -24,17 +36,16 @@ async def post_book(id: int, name: str, author: str):
     return {"Message": "Book added successfully!", "Books": books}
 
 @app.get("/books/{id}")
-async def get_book_id():
-    return {"Message":"Success", "Book": {id}}
+async def get_book_id(id: int):
+    return {"Message":"Success", "Book": id}
 
-@app.put("/books/{id}")
-async def edit_book():
+@app.put("/books/{book_id}")
+async def edit_book(book_id: int, books: Books):
     global books
-    books[id] = 
     return {"Message": "Book edited successfully!", "Books": books}
 
 @app.delete("/books/{id}")
-async def delete_book():
+async def delete_book(id: int):
     global books
     books[id] = ""
     return {"Message": "Book deleted successfully!", "Books": books}
