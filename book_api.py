a06fc00e6be1d0cd163ada268fd5beccc8e75fbd
Melyn Were <WereMA@cardiff.ac.uk>
2025-06-18 16:38:11 +0100

    finish get id request

--- book_api.py
+++ book_api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -26,20 +26,30 @@ async def add_book(book: Books):
 @app.get("/books/{book_id}")
 async def get_book_id(book_id: int):
     global book_catalog
-    results = {"book_id": book_id, "book": book_catalog[book_id]}
-    return results
+    if book_id < 0 or book_id >= len(book_catalog):
+        raise HTTPException(status_code=404, detail="Book ID does not exist")
+    else:
+        results = {"book_id": book_id, "book": book_catalog[book_id]}
+        return results
+        
 
 @app.put("/books/{book_id}")
-async def edit_book(book_id: int, books: Books):
+async def edit_book(book_id: int, book: Books):
     global book_catalog
-    book_catalog[book_id] = books
-    results = {"book_id": book_id, "books": book_catalog}
-    return results
+    book_catalog[book_id] = book
+    if book_id < 0 or book_id >= len(book_catalog):
+        raise HTTPException(status_code=404, detail="Book ID does not exist")
+    else:
+        results = {"book_id": book_id, "books": book_catalog}
+        return results
 
 @app.delete("/books/{book_id}")
 async def delete_book(book_id: int):
     global book_catalog
-    book_catalog.pop(book_id)
-    return {"Message": "Book deleted successfully!", "Books": book_catalog}
+    if book_id < 0 or book_id >= len(book_catalog):
+        raise HTTPException(status_code=404, detail="Book ID does not exist")
+    else:
+        book_catalog.pop(book_id)
+        return {"Message": "Book deleted successfully!", "books": book_catalog}
 
 
